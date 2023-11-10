4121e88e403bab2d1d552d06e12be4f64dc4b89f
Folashade Oyeleke <oyelekefola@yahoo.com>
2023-11-10 15:51:06 +0000

    Added the @Enumerated annotation to the ENUMs in the Budget entity, Added the @JsonIgnore annotation to user and expenses in the Budget entity

--- src/main/java/com/cbfacademy/apiassessment/Entity/Budget.java
+++ src/main/java/com/cbfacademy/apiassessment/Entity/Budget.java
@@ -1,5 +1,6 @@
 package com.cbfacademy.apiassessment.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,32 +10,40 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
-@Entity
-@Table(name = "Expenses")
+@Entity(name = "Budget")
+@Table(name = "Budget")
 public class Budget extends BaseEntity implements Serializable {
     @Column(name = "amount")
     private double budgetAmount;
 
     @Column(name = "category")
-    private Category budgetCategory;
+    @Enumerated(EnumType.STRING)
+    private SubCategories.Category budgetCategory;
 
     @Column(name = "subcategory", nullable = true)
-    private Subcategory budgetSubcategory;
+    @Enumerated(EnumType.STRING)
+    private SubCategories budgetSubcategory;
 
     @Column(name = "description")
     private String description;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private User user;
 
     @OneToMany(mappedBy = "budget", cascade = CascadeType.ALL)
+    @JsonIgnore
     private List<Expenses> expenses;
 
-    public Budget(double budgetAmount, Category budgetCategory, Subcategory budgetSubcategory, String description){
+    public Budget(double budgetAmount, SubCategories.Category budgetCategory, SubCategories budgetSubcategory, String description) {
         setBudgetAmount(budgetAmount);
         setBudgetCategory(budgetCategory);
         setBudgetSubcategory(budgetSubcategory);
         setDescription(description);
+    }
+
+    public String toString() {
+        return "Budget(budgetAmount=" + this.getBudgetAmount() + ", budgetCategory=" + this.getBudgetCategory() + ", budgetSubcategory=" + this.getBudgetSubcategory() + ", description=" + this.getDescription() + ", expenses=" + this.getExpenses() + ")";
     }
 }
